3653d47fff602dbc0070df56b56344feb0889b52
Pjdaze <pjdazeux@gmail.com>
2019-11-13 00:51:55 -0500

    stable

--- src/App.js
+++ src/App.js
@@ -1,26 +1,94 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import HomeWrap from "./wrappers/HomeWrap";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      active: false,
+      reset: "Reset",
+      goodNews: "Good News",
+      badNews: "Bad News",
+      giphyData: {}
+    };
+  }
+  // The tick function sets the current state. TypeScript will let us know
+
+  toggleClass = () => {
+    this.setState(prevState => ({ active: !prevState.active }));
+  };
+
+  handleBadNews = () => {
+    this.setState({
+      active: true
+    });
+  };
+
+  handleReset = () => {
+    this.setState({
+      reset: "Are You Ready To Work?",
+      active: false
+    });
+  };
+
+  handleGoodNews = () => {
+    this.setState({
+      goodNews: "Sorry Still Bad!",
+      active: true
+    });
+  };
+
+  render() {
+    const { active, reset, goodNews, badNews } = this.state;
+    return (
+      <HomeWrap>
+        <div className="wrapper">
+          <h1 className="head">SAD COOKIE</h1>
+
+          <div className="cookie">
+            <div
+              onClick={this.toggleClass}
+              className={`cookie-eye sad ${active ? "active" : null}`}
+            ></div>
+
+            <div className="left-foot"></div>
+
+            <div className="right-foot"></div>
+          </div>
+
+          {/* end of cookie*/}
+
+          {/*CONTROLS AREA*/}
+
+          <div className="news-wrapper">
+            <button onClick={this.handleBadNews}>
+              <div className="good-news news sad">
+                <h4>{badNews}</h4>
+              </div>
+            </button>
+
+            <button
+              onClick={this.handleGoodNews}
+              className="good-news news sad"
+            >
+              <h4>{goodNews}</h4>
+            </button>
+          </div>
+
+          <button
+            type="button"
+            className="reset sad"
+            onClick={this.handleReset}
+          >
+            <h4>{reset}</h4>
+          </button>
+        </div>
+      </HomeWrap>
+    );
+  }
 }
 
 export default App;
---
+++ src/wrappers/HomeWrap.js
@@ -0,0 +1,144 @@
+import styled from "styled-components";
+
+const HomeWrap = styled.div`
+  height: 1000px;
+  min-height: 100%;
+  .news-wrapper {
+    width: 99%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 50px;
+  }
+  .news {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+
+    width: 189px;
+    min-width: 185px;
+    height: 75px;
+
+    font-family: "Libre Barcode 39 Text", cursive;
+    font-size: 1.9em;
+
+    &:hover {
+      transition: all 2s ease-in-out;
+      background: linear-gradient(to right, #222, rgb(161, 88, 84));
+    }
+  }
+
+  @media only screen and (min-width: 750px) {
+    .news {
+      font-size: 2.2em;
+      margin: 35px;
+    }
+
+    .reset h4 {
+      font-size: 2em;
+      margin: 35px;
+    }
+  }
+  .good-news {
+    &:hover {
+      background: linear-gradient(to right, #222, rgb(75, 201, 255));
+    }
+  }
+
+  .reset {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #fff;
+    font-family: "Libre Barcode 39 Text", cursive;
+    font-size: 1.8em;
+
+    width: 400px;
+    min-width: 350px;
+    height: 75px;
+    font-size: 20px;
+    cursor: pointer;
+  }
+  .head {
+    font-family: "Fredoka One", cursive;
+    font-family: "Cabin Sketch", cursive;
+    font-weight: 700;
+    font-size: 3em;
+  }
+
+  .cookie {
+    position: relative;
+    width: 270px;
+    height: 355px;
+    background: #ada996; /* fallback for old browsers */
+    background: -webkit-linear-gradient(
+      to left,
+      #eaeaea,
+      #ffffff,
+      #dbdbdb,
+      #ada996
+    ); /* Chrome 10-25, Safari 5.1-6 */
+    background: linear-gradient(to left, #eaeaea, #ffffff, #dbdbdb, #ada996);
+
+    -webkit-border-radius: 63px 63px 63px 63px / 108px 108px 72px 72px;
+    border-radius: 50% 50% 35% 35% / 60% 60% 40% 40%;
+    transition: 2000ms ease-in-out;
+  }
+  .cookie-eye {
+    position: absolute;
+    left: 50%;
+    top: 20%;
+    margin-left: -10px;
+    height: 15px;
+    width: 15px;
+    border-radius: 50%;
+    box-shadow: inset 0 0 0px 3px #82e0f1;
+    transition: 3s ease-in-out;
+
+    background-color: #afffffc9;
+
+    &.active {
+      background-color: #b3b1b2;
+      box-shadow: inset 0 0 0px 3px #8a8a8a;
+    }
+  }
+
+  .cookie-rim {
+    height: 100%;
+    width: 100%;
+    box-shadow: 0 0 35px 10px #9ffef7;
+    border-radius: 50%;
+    transition: 2s ease-in-out;
+
+    &.active {
+      box-shadow: 0 0 35px 10px #fff !important;
+    }
+  }
+
+  .left-foot {
+    width: 30px;
+    height: 12px;
+    border-bottom-right-radius: 8px;
+    border-bottom-left-radius: 8px;
+    position: absolute;
+    bottom: -9px;
+    left: 26%;
+    background: linear-gradient(to bottom, #dfdfdf, #d3d2cf);
+    transform: rotate(8deg);
+  }
+
+  .right-foot {
+    width: 30px;
+    height: 12px;
+    background: linear-gradient(to bottom, #f9f9f9, #f4f0e4);
+    position: absolute;
+    bottom: -9px;
+    right: 26%;
+    border-bottom-right-radius: 8px;
+    border-bottom-left-radius: 8px;
+    transform: rotate(-8deg);
+  }
+`;
+
+export default HomeWrap;
