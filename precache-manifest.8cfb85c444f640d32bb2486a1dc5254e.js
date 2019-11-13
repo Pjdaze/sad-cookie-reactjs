b93120851b0168a46e526f55eb5d7746275e7004
Pjdaze <pjdazeux@gmail.com>
2019-11-13 01:04:34 -0500

    Updates

---
+++ precache-manifest.8cfb85c444f640d32bb2486a1dc5254e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cb75cff550502eac719335e1cb47a54",
+    "url": "/sad-cookie-reactjs/index.html"
+  },
+  {
+    "revision": "e1b2e1bf5975608c3379",
+    "url": "/sad-cookie-reactjs/static/css/main.a272b1f5.chunk.css"
+  },
+  {
+    "revision": "138a352b77dfe5bcd499",
+    "url": "/sad-cookie-reactjs/static/js/2.64a6a175.chunk.js"
+  },
+  {
+    "revision": "e1b2e1bf5975608c3379",
+    "url": "/sad-cookie-reactjs/static/js/main.1fe1dc7b.chunk.js"
+  },
+  {
+    "revision": "d21f442a2fda0b23b6bd",
+    "url": "/sad-cookie-reactjs/static/js/runtime-main.8fe47950.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/sad-cookie-reactjs/static/media/logo.25bf045c.svg"
+  }
+]);
