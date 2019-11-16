ea51682572e5109d1b30de6c489016dbad8b504b
Pjdaze <pjdazeux@gmail.com>
2019-11-16 17:36:26 -0500

    stable

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import * as React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import HomeWrap from "./wrappers/HomeWrap";
+import giphyLogo from "./assets/Poweredby_100px-Black_VertText.png";
 
 const videoStyle = {
   width: "600px",
   position: "absolute",
   left: "50%",
   marginLeft: "-300px",
+  top: "30%"
+};
+
+const giphyStyle = {
+  position: "absolute",
+  right: "100px",
+  width: "100px",
+
   top: "30%"
 };
 
@@ -21,7 +30,8 @@ export class App extends React.Component {
       goodNews: "Good News",
       badNews: "Bad News",
       giphyData: {},
-      man: ""
+      man: "",
+      attributes: ""
     };
   }
   // The tick function sets the current state. TypeScript will let us know
@@ -54,9 +64,10 @@ export class App extends React.Component {
 
   handleReset = () => {
     this.setState({
-      reset: "Are You Ready To Work",
       active: false,
-      man: this.state.giphyData.data.images.hd.mp4
+      man: this.state.giphyData.data.images.hd.mp4,
+      attributes: giphyLogo,
+      reset: "Are You Ready To Work"
     });
   };
 
@@ -68,9 +79,14 @@ export class App extends React.Component {
   };
 
   render() {
-    const { active, reset, goodNews, badNews, man } = this.state;
+    const { active, reset, goodNews, badNews, man, attributes } = this.state;
     return (
       <HomeWrap>
+        <a href="https://giphy.com/">
+          {" "}
+          <img src={attributes} style={giphyStyle} />
+        </a>
+
         <h1 className="head">SAD COOKIE</h1>
 
         <div className="cookie">
@@ -110,13 +126,15 @@ export class App extends React.Component {
         <button type="button" className="reset sad" onClick={this.handleReset}>
           <h4>{reset}</h4>
         </button>
-        <video
-          playsInline={true}
-          loop={true}
-          autoPlay
-          style={videoStyle}
-          src={man}
-        />
+        <div>
+          <video
+            playsInline={true}
+            loop={true}
+            autoPlay
+            style={videoStyle}
+            src={man}
+          />
+        </div>
       </HomeWrap>
     );
   }
